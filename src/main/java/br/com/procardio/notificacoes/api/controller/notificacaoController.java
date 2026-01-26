9c3f1d99bba002c5b3ea817ee474316950850126
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-01-26 19:47:37 -0300

    inicializando o repositório

---
+++ src/main/java/br/com/procardio/notificacoes/api/controller/notificacaoController.java
@@ -0,0 +1,40 @@
+package br.com.procardio.notificacoes.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.procardio.notificacoes.api.client.ProcardioClient;
+import br.com.procardio.notificacoes.api.dto.UsuarioDTO;
+
+@RestController
+@RequestMapping("/api/notificacoes")
+public class notificacaoController {
+
+    @Autowired
+    private ProcardioClient procardioClient;
+
+    @PostMapping("/boas-vindas")
+    public ResponseEntity<String> enviarEmailBoasVindas() {
+        List<UsuarioDTO> usuarios = procardioClient.listarUsuarios();
+
+        if (usuarios.isEmpty()) {
+            return ResponseEntity.ok("Nenhum usuário encontrado na procardio-api.");
+        }
+
+        StringBuilder logEnvio = new StringBuilder();
+
+        for (UsuarioDTO usuario : usuarios) {
+            logEnvio.append("Email de boas-vindas enviado para: ")
+                    .append(usuario.email())
+                    .append("/n");
+        }
+
+        return ResponseEntity.ok(logEnvio.toString());
+    }
+
+}
---
+++ src/test/java/br/com/procardio/notificacoes/api/NotificaoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.procardio.notificacoes.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificaoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
