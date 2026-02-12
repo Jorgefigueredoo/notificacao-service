aaa55e748db4f34f0d80687a9bf548cfbe9eb07d
Jorgefigueredoo <jorgeafigueredo@gmail.com>
2026-02-11 21:48:16 -0300

    ...

--- src/main/java/br/com/procardio/notificacoes/api/client/ProcardioClient.java
+++ src/main/java/br/com/procardio/notificacoes/api/client/ProcardioClient.java
@@ -4,13 +4,16 @@ import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import br.com.procardio.notificacoes.api.dto.UsuarioDTO;
 
 @FeignClient(name = "procardio-api", url = "http://localhost:8080")
 public interface ProcardioClient {
-    
-    @GetMapping("/api/usuarios")
-    List <UsuarioDTO> listarUsuarios();
 
+    @GetMapping("/api/usuarios")
+    List<UsuarioDTO> listarUsuarios();
+
+    @GetMapping("/api/usuarios/{id}")
+    UsuarioDTO buscarUsuarioPorId(@PathVariable("id") Long id);
 }
